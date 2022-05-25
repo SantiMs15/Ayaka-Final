72d7915d6587912ff8749e75e5b3dd88f4ab465b
Chokoramo <vzchoko15@gmail.com>
2022-05-25 17:07:04 -0500

    Bienvenida

--- ayakaf.py
+++ ayakaf.py
@@ -19,4 +19,9 @@ async def on_message(message):
     if message.content.startswith('!Hello Ayaka'):
         await message.channel.send("Hi I can translate whatever you want!")
 
+@client.event
+async def on_member_join(member):
+    await member.create.dm()
+    await member.dm_channel.send(f"Bienvenido seas {member}. Este es el servidor de prueba de nuestra bot Ayaka, que esperamos que nos haga pasar la materia :v")
+
 client.run(TOKEN) 
