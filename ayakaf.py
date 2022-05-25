2f1545c62beaeba6e598a4ba6eb6601c15b14a54
Chokoramo <vzchoko15@gmail.com>
2022-05-25 16:28:23 -0500

    Push 1

--- ayakaf.py
+++ ayakaf.py
@@ -1,4 +1,3 @@
-from http import client
 import os
 import discord
 from dotenv import load_dotenv
@@ -7,6 +6,14 @@ client = discord.Client()
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!translate'):
+        await message.channel.send("I can translate whatever you want!")
 
 @client.event
 async def on_connect():
