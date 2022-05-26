f5f752885f254b5141b6978432fbd6495e1a473a
David Santiago Guzmán Ruiz <adanbot23@gmail.com>
2022-05-25 20:19:45 -0500

    super change

--- ayakaf.py
+++ ayakaf.py
@@ -1,31 +1,5 @@
-from imaplib import _CommandResults
-import googletrans
-from googletrans import Translator
-import os
 import discord
-from dotenv import load_dotenv
+import googletrans
 from discord.ext import commands
-client = discord.Client()
-
-load_dotenv()
-TOKEN = os.getenv('TOKEN')
-
-@client.event
-async def on_ready():
-    print(f"Bot logged in as {client.user}, Lets Translate!")
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!Hello Ayaka'):
-        await message.channel.send("Hi I can translate whatever you want!")
-
-@client.event
-async def on_member_join(member):
-    await member.create.dm()
-    await member.dm_channel.send(f"Bienvenido seas {member}. Este es el servidor de prueba de nuestra bot Ayaka, que esperamos que nos haga pasar la materia :v")
-
-client.run(TOKEN) 
+client=discord.Client()
 
---
+++ prieba.py
@@ -0,0 +1,29 @@
+import json
+import TranslatorApi
+import os
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+client = discord.Client()
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+@client.event
+async def on_ready():
+    print(f"Bot logged in as {client.user}, Lets Translate!")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!Hello Ayaka'):
+        await message.channel.send("Hi I can translate whatever you want!")
+
+@client.event
+async def on_member_join(member):
+    await member.create.dm()
+    await member.dm_channel.send(f"Bienvenido seas {member}. Este es el servidor de prueba de nuestra bot Ayaka, que esperamos que nos haga pasar la materia :v")
+
+client.run(TOKEN) 
